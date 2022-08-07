bcf972520c941d933ff3d7fab0b1d00996ac85d7
JonOfUs <11487762+JonOfUs@users.noreply.github.com>
2022-08-07 14:51:21 +0200

    Added CSP exception for images on settings page, so that podcast logos load

--- lib/Settings/GPodderSyncPersonal.php
+++ lib/Settings/GPodderSyncPersonal.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace OCA\GPodderSync\Settings;
 
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\Settings\ISettings;
@@ -11,7 +12,13 @@ use OCP\Settings\ISettings;
 class GPodderSyncPersonal implements ISettings {
 
 	public function getForm(): TemplateResponse {
-		return new TemplateResponse('gpoddersync', 'settings/personal', []);
+		$response = new TemplateResponse('gpoddersync', 'settings/personal', []);
+		
+		$csp = new ContentSecurityPolicy();
+		$csp->addAllowedImageDomain('*');
+		$response->setContentSecurityPolicy($csp);
+		
+		return $response;
 	}
 
 	public function getSection(): string {
